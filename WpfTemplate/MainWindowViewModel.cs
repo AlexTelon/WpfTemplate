3c5d083e88ebf48b7718fe58488c4f57fd62378e
Alex Telon <alex.telon@gmail.com>
2018-06-23 16:52:15 +0200

    Sample bindings
    A sample string and observablecollection binding.

--- WpfTemplate/MainWindowViewModel.cs
+++ WpfTemplate/MainWindowViewModel.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfTemplate.Utils;
 
 namespace WpfTemplate
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : BindableBase
     {
+        public string HelloWorld
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public ObservableCollection<string> CollectionOfWords { get; set; } = new ObservableCollection<string>() { "a", "few", "words" };
+
+        public MainWindowViewModel()
+        {
+            HelloWorld = "hello world test string";
+        }
     }
 }
